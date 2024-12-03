434ee0859cc9793fc93476a5da2e993e5df4e4d2
Algebras <181668241+teamalgebras@users.noreply.github.com>
2024-12-03 22:37:43 +0400

    Dist folder

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,23 +5,20 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [starlight({
-        title: 'My Docs',
-        social: {
-            github: 'https://github.com/withastro/starlight',
+  integrations: [
+    starlight({
+      title: 'Algebras',
+      sidebar: [
+        {
+          label: 'Guides',
+          items: [{ label: 'Example Guide', slug: 'guides/example' }],
         },
-        sidebar: [
-            {
-                label: 'Guides',
-                items: [
-                    // Each item here is one entry in the navigation menu.
-                    { label: 'Example Guide', slug: 'guides/example' },
-                ],
-            },
-            {
-                label: 'Reference',
-                autogenerate: { directory: 'reference' },
-            },
-        ],
-		}), react()],
+        {
+          label: 'Reference',
+          autogenerate: { directory: 'reference' },
+        },
+      ],
+    }),
+    react(),
+  ],
 });
