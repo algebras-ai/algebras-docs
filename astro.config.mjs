8616eab560c9da3a8831c69012a7930cd26875d7
Algebras <181668241+teamalgebras@users.noreply.github.com>
2024-12-03 23:48:30 +0400

    New base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import react from '@astrojs/react';
 // https://astro.build/config
 export default defineConfig({
   outDir: './docs',
-  base: '/algebras-docs',
+  // base: '/algebras-docs',
   build: {
     assets: 'app_assets',
   },
